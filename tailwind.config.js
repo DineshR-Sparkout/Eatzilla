32a5645d87ef0673d1c7ac9d5e001b9cb104622c
Dinesh-R <dinesh@sparkouttech.com>
2024-07-01 19:23:46 +0530

    commit changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,42 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
     theme: {
-    extend: {},
+    extend: {
+      screens: {
+        xs:'400px',
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1200px',
+        '2xl': '1440px',
+      },
+      container: {
+        center: true,
+        screens: {
+          sm: '600px',
+          md: '720px',
+          lg: '960px',
+          xl:'1140px',
+          '2xl':'1140px'
+        },
+        padding:'15px',
+      },
+      colors:{
+        'midnight':'#1F2A37',
+        'link-water':'#D1D5DB',
+        'soap-stone':'#111928',
+        'dark-cerulean':'#008352',
+        'saddle':'#249641',
+        'catalina-blue':'#6B7280',
+        'gulf-blue':'#9CA3AF',
+        'alice-blue':'#F9FAFB',
+        'ghost-white':'#FAFAFA'
+      },
+      fontFamily:{
+        Inter:'Inter',
+        poppins:'Poppins',
+      }
+    },
   },
   plugins: [],
 }
-
